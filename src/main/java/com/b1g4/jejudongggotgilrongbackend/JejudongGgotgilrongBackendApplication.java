158aabae7b345a7c19d58b781eab38f4a66bf32e
jeonyuneo <piq2255@gmail.com>
2023-07-06 02:28:32 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/b1g4/jejudongggotgilrongbackend/JejudongGgotgilrongBackendApplication.java
@@ -0,0 +1,12 @@
+package com.b1g4.jejudongggotgilrongbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JejudongGgotgilrongBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JejudongGgotgilrongBackendApplication.class, args);
+    }
+}
---
+++ src/test/java/com/b1g4/jejudongggotgilrongbackend/JejudongGgotgilrongBackendApplicationTests.java
@@ -0,0 +1,12 @@
+package com.b1g4.jejudongggotgilrongbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JejudongGgotgilrongBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
