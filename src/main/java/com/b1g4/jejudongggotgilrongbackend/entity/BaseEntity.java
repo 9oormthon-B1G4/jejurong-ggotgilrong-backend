94af59229e52491d6f0942150ec26e9455cc4096
jeonyuneo <piq2255@gmail.com>
2023-07-06 22:32:51 +0900

    feat: BaseEntity id 타입 수정

--- src/main/java/com/b1g4/jejudongggotgilrongbackend/entity/BaseEntity.java
+++ src/main/java/com/b1g4/jejudongggotgilrongbackend/entity/BaseEntity.java
@@ -22,7 +22,7 @@ public abstract class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     @CreatedDate
     private LocalDateTime createdDate;
