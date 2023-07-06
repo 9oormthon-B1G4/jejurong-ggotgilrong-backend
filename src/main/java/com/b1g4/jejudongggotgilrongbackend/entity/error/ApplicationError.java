13a1a6e4cba3b220d80a4edcad8d8f0557e60309
jeonyuneo <piq2255@gmail.com>
2023-07-06 22:24:40 +0900

    feat: ApplicationError 추가

---
+++ src/main/java/com/b1g4/jejudongggotgilrongbackend/entity/error/ApplicationError.java
@@ -0,0 +1,18 @@
+package com.b1g4.jejudongggotgilrongbackend.entity.error;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ApplicationError {
+
+    ROUTE_NOT_FOUND(HttpStatus.BAD_REQUEST, "R001", "노선을 찾을 수 없습니다."),
+
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S001", "서버 내부 에러가 발생했습니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+}
