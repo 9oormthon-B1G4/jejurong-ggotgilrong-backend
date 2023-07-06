6f6fe2348ce641e88c58b657a15b358dd01f3589
jeonyuneo <piq2255@gmail.com>
2023-07-06 15:20:29 +0900

    feat: BaseEntity 추가

---
+++ src/main/java/com/b1g4/jejudongggotgilrongbackend/entity/BaseEntity.java
@@ -0,0 +1,40 @@
+package com.b1g4.jejudongggotgilrongbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@SuperBuilder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+    private LocalDateTime deletedDate;
+    private boolean isDeleted;
+
+    public void delete() {
+        isDeleted = true;
+        deletedDate = LocalDateTime.now();
+    }
+}
