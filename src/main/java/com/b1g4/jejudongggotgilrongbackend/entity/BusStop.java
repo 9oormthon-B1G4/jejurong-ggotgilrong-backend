2abb0c9c85e78cb6d6a39eedba590d9ab75210a0
jeonyuneo <piq2255@gmail.com>
2023-07-06 15:21:52 +0900

    feat: BusStop 추가

---
+++ src/main/java/com/b1g4/jejudongggotgilrongbackend/entity/BusStop.java
@@ -0,0 +1,31 @@
+package com.b1g4.jejudongggotgilrongbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@SuperBuilder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AttributeOverride(name = "id", column = @Column(name = "bus_stop_id"))
+@Entity
+public class BusStop extends BaseEntity {
+
+    private String name;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "busStop", cascade = CascadeType.MERGE)
+    private List<BusStopRoute> busStopRoutes = new ArrayList<>();
+
+    @Builder.Default
+    @OneToMany(mappedBy = "busStop", cascade = CascadeType.MERGE)
+    private List<GuestBook> guestBooks = new ArrayList<>();
+
+    @Builder.Default
+    @OneToMany(mappedBy = "busStop", cascade = CascadeType.MERGE)
+    private List<Place> places = new ArrayList<>();
+}
