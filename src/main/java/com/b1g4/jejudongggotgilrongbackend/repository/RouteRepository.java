a49a02f5b60ec8ef7ef06bedc0237900ae2bd884
jeonyuneo <piq2255@gmail.com>
2023-07-07 00:38:11 +0900

    feat: RouteRepository 추가

---
+++ src/main/java/com/b1g4/jejudongggotgilrongbackend/repository/RouteRepository.java
@@ -0,0 +1,7 @@
+package com.b1g4.jejudongggotgilrongbackend.repository;
+
+import com.b1g4.jejudongggotgilrongbackend.entity.Route;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RouteRepository extends JpaRepository<Route, Long> {
+}
