6252b82e5316895fb8afb20f97318dc901aa9ed6
jeonyuneo <piq2255@gmail.com>
2023-07-07 00:55:15 +0900

    style: GuestBookRepository 주석 삭제

--- src/main/java/com/b1g4/jejudongggotgilrongbackend/repository/GuestBookRepository.java
+++ src/main/java/com/b1g4/jejudongggotgilrongbackend/repository/GuestBookRepository.java
@@ -9,14 +9,6 @@ import java.util.List;
 
 public interface GuestBookRepository extends JpaRepository<GuestBook, Long> {
 
-//    @Query(value = "select res.bus_stop_id, name, content " +
-//            "from (select name, number, bus_stop_id from route join bus_stop_route on :routeId = bus_stop_route.route_id) res, guest_book" +
-//            " where guest_book.bus_stop_id = res.bus_stop_id " +
-//            " order by created_date desc " +
-//            "limit 5",
-//            nativeQuery = true)
-//    List<GuestBook> findByRouteOrderByCreatedDateDesc(@Param("routeId") Long routeId);
-
     List<GuestBook> findByRouteIdOrderByCreatedDateDesc(Long routeId);
 
     List<GuestBook> findByBusStopIdOrderByCreatedDateDesc(Long busStopId);
